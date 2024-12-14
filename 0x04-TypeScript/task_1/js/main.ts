7cf7f3cd1041601159806d994a6e95c824264399
Brian Wakhale <brianweke1998@gmail.com>
2024-12-14 07:56:48 +0300

    Created a Print Function that accepts two arguments firstName and lastName

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,6 +10,15 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number; // Required attribute
 }
+// Define the printTeacherFunction interface
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string; // Function signature
+}
+// Implement the printTeacher function
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
 const director1: Directors = {
   firstName: 'Tracy',
   lastName: 'Cheptoo',
@@ -17,6 +26,9 @@ const director1: Directors = {
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
+// Call printTeacher with director1's name
+console.log(printTeacher(director1.firstName, director1.lastName)); // Output: T. Cheptoo
+
 //log Director to the console
 console.log(director1);
 
